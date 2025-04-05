2f88753cd5d00d958ef241507f53e8cd44a8c187
Vaibhav Bhagare <bhagare.vaibhav@gmail.com>
2025-04-05 13:47:09 +0530

    added vercel

--- server.js
+++ server.js
@@ -16,8 +16,8 @@ mongoose.connect(process.env.MONGO_URI, {
 })
 .then(() => {
   console.log('MongoDB connected');
-  app.listen(process.env.PORT, () => {
-    console.log(`Server running on port ${process.env.PORT}`);
+  app.listen(process.env.PORT || 8080, () => {
+    console.log(`Server running on port ${process.env.PORT || 8080}`);
   });
 })
 .catch(err => console.log(err));
