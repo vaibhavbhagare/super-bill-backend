53719904e680084c1cd1baffacfc3cf7e47a030b
Vaibhav Bhagare <bhagare.vaibhav@gmail.com>
2025-05-11 12:16:23 +0530

    added cors

--- server.js
+++ server.js
@@ -2,16 +2,24 @@ const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const bodyParser = require('body-parser');
+const cors = require('cors'); // Import cors middleware
 const userRoutes = require('./routes/userRoutes');
 const customerRoutes = require('./routes/customerRoutes');
 
 dotenv.config();
 
 const app = express();
+
+// Enable CORS and allow requests from http://localhost:8081
+app.use(cors({
+  origin: 'http://localhost:8081', // Allow only this origin
+  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed HTTP methods
+  allowedHeaders: ['Content-Type', 'Authorization'], // Allowed headers
+}));
+
 app.use(bodyParser.json());
 app.use('/api/users', userRoutes);
 app.use('/api/customers', customerRoutes);
-
 
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
