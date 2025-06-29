1a4f2a603caf8fc6e0a0d2301a5ee8a376921785
Vaibhav Bhagare <bhagare.vaibhav@gmail.com>
2025-06-29 14:14:29 +0530

    trust proxy

--- server.js
+++ server.js
@@ -29,7 +29,9 @@ app.use(
   }),
 );
 
-// Rate limiting
+app.set('trust proxy', true);
+
+// Apply rate limiting
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
   max: 100, // limit each IP to 100 requests per windowMs
