ff7d0653185d0e7d50a567391d2a428dabd0c5a4
Vaibhav Bhagare <bhagare.vaibhav@gmail.com>
2025-07-15 19:35:29 +0530

    added expiryDate in product

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ app.use(
 // Apply rate limiting
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 500, // limit each IP to 100 requests per windowMs
+  max: 5000, // limit each IP to 100 requests per windowMs
 });
 app.use(limiter);
 
