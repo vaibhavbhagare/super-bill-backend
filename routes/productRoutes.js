9af4d3ae38995f04ed825517cbffaaf38dc45b30
Vaibhav Bhagare <bhagare.vaibhav@gmail.com>
2025-07-16 14:53:43 +0530

    Added csv export

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -9,6 +9,8 @@ router.use(auth);
 // Product routes
 router.post("/", productController.createProduct);
 router.get("/", productController.getProducts);
+router.get("/export-csv", productController.exportCsv);
+
 router.get("/:id", productController.getProductById);
 router.put("/:id", productController.updateProduct);
 router.delete("/:id", productController.deleteProduct);
