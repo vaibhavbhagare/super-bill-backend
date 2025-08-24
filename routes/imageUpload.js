0071d10b9d31f57c6c77aefc0fd0030f03797c9d
Vaibhav Bhagare <bhagare.vaibhav@gmail.com>
2025-08-24 11:49:11 +0530

    memoryStorage

--- routes/imageUpload.js
+++ routes/imageUpload.js
@@ -4,13 +4,24 @@ const multer = require("multer");
 const imageUploadController = require("../controllers/imageUploadController");
 const { auth } = require("../middleware/auth");
 
-
-const upload = multer({ dest: "uploads/" });
+const upload = multer({
+  storage:
+    process.env.NODE_ENV === "production"
+      ? multer.memoryStorage()
+      : multer.diskStorage({ dest: "./uploads/" }),
+  limits: {
+    fileSize: 5 * 1024 * 1024,
+  },
+});
 
 // Protected routes (authentication required)
 router.use(auth); // Apply auth middleware to all routes below
- 
-router.post("/upload/:productId", upload.single("image"), imageUploadController.uploadProductImage);
+
+router.post(
+  "/upload/:productId",
+  upload.single("image"),
+  imageUploadController.uploadProductImage
+);
 
 // Delete image
 router.delete("/:productId", imageUploadController.deleteProductImage);
